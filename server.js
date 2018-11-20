7fa01d99f1a03991a1b8d67545ca859d8eeb13f0
Ori Sayda <orisayda@gmail.com>
2018-11-20 16:52:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var path = require('path');
 
 
 //Allow all requests from all domains & localhost
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
   next();
 });
